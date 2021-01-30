8b1b8d265153bae2e528e1b78b7f85f772cef986
Alexey Dronov <alexadronov@gmail.com>
2021-01-30 23:09:55 +0300

    New updates for Heroku deployment 14

--- conf/settings.py
+++ conf/settings.py
@@ -175,4 +175,4 @@ LOGGING = {
 DEBUG_PROPAGATE_EXCEPTIONS = True
 COMPRESS_ENABLED = os.environ.get('COMPRESS_ENABLED', False)
 
-django_heroku.settings(locals())
+django_heroku.settings(config=locals(), staticfiles=False, logging=False)
